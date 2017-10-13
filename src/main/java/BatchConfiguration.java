1a1cc89290ef2735ceb06f8a01641ad6eb4fe7ed
fchambon <fchambon@objectif.paris>
2017-10-13 11:37:19 +0200

    Mise en place de l'archi des ItemComponents - ItemReader - ItemProcessor - ItemWriter

--- src/main/java/BatchConfiguration.java
+++ src/main/java/BatchConfiguration.java
@@ -1,8 +1,37 @@
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @EnableBatchProcessing
 @Configuration
 public class BatchConfiguration {
 
+	@Bean
+	public ItemReader<Object> itemReader(){
+		return null;
+	}
+	
+	@Bean
+	public ItemProcessor<Object,Object> itemProcessor(){
+		return null;
+	}
+	
+	public ItemWriter<Object> itemWriter(){
+		return null;
+	}
+	
+	@Bean
+	public Step mouvementStep(StepBuilderFactory stepBuilderFactory,ItemReader<Object> itemReader,ItemProcessor<Object,Object> itemProcessor, ItemWriter<Object> itemWriter){
+		return stepBuilderFactory
+				.get("smartContractStep").<Object,Object> chunk(100)
+				.reader(itemReader)
+				.processor(itemProcessor)
+				.writer(itemWriter)
+				.build();
+	}
 }
